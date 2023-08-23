d79ed209fb8c0cc656570f1bcb5fe960231f4978
junaidk20 <junaidkhan803061@gmail.com>
2023-08-23 22:29:26 +0530

    .

---
+++ questions.js
@@ -0,0 +1,31 @@
+//Q1:add two numbers
+function add(a,b){
+return a+b;
+}
+console.log(add(3,5));
+//Q2:multiply two numbers
+function multiply(a,b){
+    return a*b;
+}
+console.log(multiply(3,9));
+//Q3:double a number
+function double(z){
+return 2*z;
+}
+console.log(double(8))
+//Q4:divide a number by a number
+function divide(n,x){
+    return n/x;
+}
+console.log(divide(6,3));
+//Q5:convert minutes into seconds
+function convert(x){
+    return  x*60;
+}
+console.log(convert(5));
+//Q6:Return the Next Number from the Integer Passed
+function nextNum(sum){
+    return 1+sum
+    
+}
+console.log(nextNum(19));
